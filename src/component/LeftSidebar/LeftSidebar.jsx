07ee7e884fa1bcd8455b2817f712f14d76fc02be
yared aregayehu <yaredbaby21@gmail.com>
2026-02-19 13:28:51 -0600

    new file added on LeftSidebar

--- src/component/LeftSidebar/LeftSidebar.jsx
+++ src/component/LeftSidebar/LeftSidebar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './LeftSidebar.css'
+import assets from '../../assets/assets';
 const LeftSidebar = () => {
   return (
-    <div>LeftSidebar</div>
-  )
+    <div className="ls">
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} className="logo" alt="" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+          </div>
+        </div>
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="" />
+          <input type="text" placeholder='search here..' />
+        </div>
+      </div>
+      <div className="ls-list">
+        <div className="friends">
+          <img src={assets.profile_img} alt="" /><div>
+            <p>Yared Aregayehu</p>
+            <span>Hello, How are you?</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LeftSidebar
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Chat.css'
+import LeftSidebar from "../../component/LeftSidebar/LeftSidebar.jsx";
+import ChatBox from '../../component/ChatBox/ChatBox'
+import RightSidebar from '../../component/RightSidebar/RightSidebar'
 
 const Chat = () => {
   return (
-    <div className='chat'>
-<div className="chat-container"> </div>
+    <div className="chat">
+      <div className="chat-container"> </div>
+      <LeftSidebar />
+      <ChatBox />
+      <RightSidebar />
     </div>
-  )
+  );
 }
 
 export default Chat
