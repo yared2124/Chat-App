f35cd90996d2503fbad1873832d592d06cb1ff73
yared aregayehu <yaredbaby21@gmail.com>
2026-02-20 04:23:38 -0600

    chatbox file inserted

--- src/component/ChatBox/ChatBox.jsx
+++ src/component/ChatBox/ChatBox.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './ChatBox.css'
+import assets from '../../assets/assets'
 const ChatBox = () => {
   return (
-    <div></div>
+    <div className='chat-box'>
+<div className="chat-user">
+  <img src={assets.profile_img} alt="" />
+  <p>yared aregayehu <img src={assets.green_dot} alt="" /></p>
+  <img src={assets.help_icon} className='' alt="" />
+
+</div>
+    </div>
   )
 }
 
