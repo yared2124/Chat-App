784488ffb586fa320e6890650d0b79a92f0c3f81
yared aregayehu <yaredbaby21@gmail.com>
2026-02-21 14:21:35 -0600

    file added to chatbox

--- src/component/ChatBox/ChatBox.jsx
+++ src/component/ChatBox/ChatBox.jsx
@@ -6,9 +6,17 @@ const ChatBox = () => {
     <div className='chat-box'>
 <div className="chat-user">
   <img src={assets.profile_img} alt="" />
-  <p>yared aregayehu <img src={assets.green_dot} alt="" /></p>
-  <img src={assets.help_icon} className='' alt="" />
+  <p>yared aregayehu <img className='dot' src={assets.green_dot} alt="" /></p>
+  <img src={assets.help_icon} className=' help' alt="" />
 
+</div>
+<div className="chat-input">
+  <input type="text" placeholder='send a message' />
+  <input type="text" id='image' accept='image/png, image/jpng' hidden />
+  <label htmlFor="image">
+    <img src={assets.gallery_icon} alt="" />
+  </label>
+  <img src={assets.send_button} alt="" />
 </div>
     </div>
   )
