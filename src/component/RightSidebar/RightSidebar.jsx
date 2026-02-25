4c46abb3135f6d818544f92e477aa9765bfd3fa2
yared aregayehu <yaredbaby21@gmail.com>
2026-02-25 10:44:32 -0600

    update /src/component/RightSidebar/RightS

--- src/component/RightSidebar/RightSidebar.jsx
+++ src/component/RightSidebar/RightSidebar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import './RightSidebar.css'
+import React from "react";
+import assets from "../../assets/assets";
+import "./RightSidebar.css";
 const RightSidebar = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="rs">
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>
+          yared Aregayehu <img src={assets.green_dot} className="dot" alt="" />
+        </h3>
+        <p>Hey, There i am yared using chat app </p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>media</p>
+        <img src={assets.pic1} alt="" />
+        <img src={assets.pic2} alt="" />
+        <img src={assets.pic3} alt="" />
+        <img src={assets.pic4} alt="" />
+        <img src={assets.pic1} alt="" />
+        <img src={assets.pic2} alt="" />
+      </div>
+      <button >Log out</button>
+    </div>
+  );
+};
 
-export default RightSidebar
+export default RightSidebar;
