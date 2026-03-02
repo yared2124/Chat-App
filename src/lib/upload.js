460cfa3c108f406ef7c25cc27a80c0ea559d858a
yared aregayehu <yaredbaby21@gmail.com>
2026-03-02 12:18:24 -0600

    upload.js file inserted & changed

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -4,8 +4,6 @@ import {
   uploadBytesResumable,
   getDownloadURL,
 } from "firebase/storage";
-import { resolvePath } from "react-router-dom";
-
 
 const upload = async (file) => {
 
@@ -13,32 +11,35 @@ const upload = async (file) => {
     const storageRef = ref(storage, `images/${Date.now()+ file.name}`);
 
     const uploadTask = uploadBytesResumable(storageRef, file);
+    return new Promise((resolve, reject) => {
+         uploadTask.on(
+           "state_changed",
+           (snapshot) => {
+             const progress =
+               (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+             console.log("Upload is " + progress + "% done");
+             switch (snapshot.state) {
+               case "paused":
+                 console.log("Upload is paused");
+                 break;
+               case "running":
+                 console.log("Upload is running");
+                 break;
+             }
+           },
+           (error) => {
+             console.log(error);
+           },
+           () => {
+             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+               resolve(downloadURL);
+             });
+           },
+         );
 
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
-        switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
-            break;
-          case "running":
-            console.log("Upload is running");
-            break;
-        }
-      },
-      (error) => {
+    })
 
-       
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        resolve(downloadURL);
-        });
-      },
-    );
+   
 
 
 }
