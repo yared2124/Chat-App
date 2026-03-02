cd753ea51cd8429ce470ca82e968fa1492045fbf
yared aregayehu <yaredbaby21@gmail.com>
2026-03-02 09:08:45 -0600

    upload.js updated

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -4,34 +4,44 @@ import {
   uploadBytesResumable,
   getDownloadURL,
 } from "firebase/storage";
-
-const storage = getStorage();
-const storageRef = ref(storage, "images/rivers.jpg");
-
-const uploadTask = uploadBytesResumable(storageRef, file);
+import { resolvePath } from "react-router-dom";
 
 
-uploadTask.on(
-  "state_changed",
-  (snapshot) => {
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log("Upload is " + progress + "% done");
-    switch (snapshot.state) {
-      case "paused":
-        console.log("Upload is paused");
-        break;
-      case "running":
-        console.log("Upload is running");
-        break;
-    }
-  },
-  (error) => {
-    
-  },
-  () => {
+const upload = async (file) => {
 
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      console.log("File available at", downloadURL);
-    });
-  },
-);
+    const storage = getStorage();
+    const storageRef = ref(storage, `images/${Date.now()+ file.name}`);
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+        }
+      },
+      (error) => {
+
+        console.error("Upload error:", error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        resolve(downloadURL);
+        });
+      },
+    );
+
+
+}
+export default upload;
+
+
