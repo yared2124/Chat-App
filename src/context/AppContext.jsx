c08353e4e8b6632b72348eb31035034f832f11de
yared aregayehu <yaredbaby21@gmail.com>
2026-03-06 12:54:55 -0600

    file updated

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -4,10 +4,9 @@ import { auth, db } from "../config/firebase.js";
 import { useNavigate } from "react-router-dom";
 import { onSnapshot } from "firebase/firestore";
 
-export const AppContext = createContext();
+const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-
   const navigate = useNavigate();
   const [userData, setUserData] = useState(null);
   const [chatData, setChatData] = useState(null);
@@ -17,37 +16,34 @@ const AppContextProvider = (props) => {
       const userRef = doc(db, "users", uid);
       const userSnap = await getDoc(userRef);
       const userData = userSnap.data();
-      if(userData.avatar && userData.name){
-      navigate("/chat");
-      } else{
+      if (userData.avatar && userData.name) {
+        navigate("/chat");
+      } else {
         navigate("/profile");
       }
-      await updateDoc(userRef,{
-        lastSeen: Date.now()
-      })
+      await updateDoc(userRef, {
+        lastSeen: Date.now(),
+      });
 
-      setInterval(async() =>{
-        if(auth.chatUser)
-        await updateDoc(userRef, { lastSeen: Date.now() });
-
-      },60000)
+      setInterval(async () => {
+        if (auth.chatUser) await updateDoc(userRef, { lastSeen: Date.now() });
+      }, 60000);
     } catch (error) {
       console.log(error);
     }
   };
 
-  useEffect(()=>
-    {
-     if (userData) {
-      const chatRef = doc(db,'chats',userData.id);
-      const unSub = onSnapshot(chatRef,async (res)=>{
-      const chatItems = res.data().chatsDate;
-      
-
-     })
-  }
-},[userData])
-
+  useEffect(() => {
+    if (userData) {
+      const chatRef = doc(db, "chats", userData.id);
+      const unSub = onSnapshot(chatRef, async (res) => {
+        const chatItems = res.data().chatsData;
+        const tempData = [];
+        for (const item of chatItems) {
+        }
+      });
+    }
+  }, [userData]);
 
   const value = {
     userData,
@@ -55,11 +51,10 @@ const AppContextProvider = (props) => {
     chatData,
     setChatData,
     loadUserData,
-    
   };
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
 };
-export default AppContextProvider;
+export { AppContext, AppContextProvider };
