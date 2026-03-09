dc9ba862cff8a1e646a591e64e797623a5adfbfa
yared aregayehu <yaredbaby21@gmail.com>
2026-03-09 15:59:47 -0500

    file inserted

--- src/component/LeftSidebar/LeftSidebar.jsx
+++ src/component/LeftSidebar/LeftSidebar.jsx
@@ -1,28 +1,50 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useState } from "react";
 import "./LeftSidebar.css";
 import assets from "../../assets/assets";
 import { useNavigate } from "react-router-dom";
-import { collection, query, where } from "firebase/firestore";
-import { db } from "../../config/firebase"
+import { collection, doc, query, where } from "firebase/firestore";
+import { db } from "../../config/firebase";
 import { getDocs } from "firebase/firestore";
-
+import { AppContext } from "../../context/AppContext";
 
 const LeftSidebar = () => {
   const navigate = useNavigate();
+  const { userData } = useContext(AppContext);
+  const [user, setUser] = useState(null);
+  const [showSearch, setShowSearch] = useState(false);
 
   const inputHandler = async (e) => {
     try {
       const input = e.target.value;
-      const userRef = collection(db, "users");
-      const q = query(userRef, where("username", "==", input.toLowerCase()));
-      const querySnap = await getDocs(q);
-      if (!querySnap.empty) {
-        console.log(querySnap.docs[0].data());
+      if (input) {
+        setShowSearch(true);
+        const userRef = collection(db, "users");
+        const q = query(userRef, where("username", "==", input.toLowerCase()));
+        const querySnap = await getDocs(q);
+        if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
+          setUser(querySnap.docs[0].data());
+        } else {
+          setUser(null);
+        }
+      } else {
+        setShowSearch(false);
       }
     } catch (error) {
       console.log(error);
     }
-  };
+  }
+
+  const addChat = async () =>{
+    const messagesRef = collection(db, "messages");
+    const chatsRef = collection(db, "chats"); 
+    try {
+      const newMessagesRef = doc(messagesRef)
+      const 
+    } catch (error) {
+      
+    }
+  }
 
   return (
     <div className="ls">
@@ -48,17 +70,24 @@ const LeftSidebar = () => {
         </div>
       </div>
       <div className="ls-list">
-        {Array(12)
-          .fill("")
-          .map((item, index) => (
-            <div key={index} className="friends">
-              <img src={assets.profile_img} alt="" />
-              <div>
-                <p>Friend {index + 1}</p>
-                <span>Hello, How are you?</span>
+        {showSearch && user ? (
+          <div onClick={addChat} className="friends add-user">
+            <img src={user.avatar} alt="" />
+            <p>{user.name}</p>
+          </div>
+        ) : (
+          Array(12)
+            .fill("")
+            .map((item, index) => (
+              <div key={index} className="friends">
+                <img src={assets.profile_img} alt="" />
+                <div>
+                  <p>Friend {index + 1}</p>
+                  <span>Hello, How are you?</span>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+        )}
       </div>
     </div>
   );
