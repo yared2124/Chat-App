bed41b9bbe49b64e6134912465a3f3c21a200cd7
yared aregayehu <yaredbaby21@gmail.com>
2026-03-10 15:37:53 -0500

    Chat.jsx modified

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './Chat.css'
 import LeftSidebar from "../../component/LeftSidebar/LeftSidebar.jsx";
 import ChatBox from '../../component/ChatBox/ChatBox'
 import RightSidebar from '../../component/RightSidebar/RightSidebar'
+import { AppContext } from '../../context/AppContext.jsx';
 
 const Chat = () => {
+
+  const {chatData,userData} = useContext(AppContext)
+  const [loading, setLoading] = useState(true)
+
   return (
     <div className="chat">
-      <div className="chat-container">
-        <LeftSidebar />
-        <ChatBox />
-        <RightSidebar />
-      </div>
+      {loading ? (
+        <p className="loading">Loading...</p>
+      ) : (
+        <div className="chat-container">
+          <LeftSidebar />
+          <ChatBox />
+          <RightSidebar />
+        </div>
+      )}
     </div>
   );
 }
