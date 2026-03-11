da345e9adeba27e7958f720d61659a4f102424bb
yared aregayehu <yaredbaby21@gmail.com>
2026-03-11 16:33:09 -0500

    LeftSidebar.jsx updated

--- src/component/LeftSidebar/LeftSidebar.jsx
+++ src/component/LeftSidebar/LeftSidebar.jsx
@@ -20,7 +20,7 @@ import { toast } from "react-toastify";
 
 const LeftSidebar = () => {
   const navigate = useNavigate();
-  const { userData, chatData } = useContext(AppContext);
+  const { userData, chatData,chatUser,setChatUser,setMessagesId,messagesId } = useContext(AppContext);
   const [user, setUser] = useState(null);
   const [showSearch, setShowSearch] = useState(false);
 
@@ -93,7 +93,8 @@ const LeftSidebar = () => {
   };
 
   const setChat = async (item) => {
-    console.log(item);
+   setMessagesId(item.messagesId);
+   setChatUser(item)
   };
 
   return (
