ee4ba364de83f3f71073de06ff80aa71c180cc29
yared aregayehu <yaredbaby21@gmail.com>
2026-03-18 10:51:43 -0500

    RightSidebar.jsx updated

--- src/component/RightSidebar/RightSidebar.jsx
+++ src/component/RightSidebar/RightSidebar.jsx
@@ -1,30 +1,54 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import assets from "../../assets/assets";
 import "./RightSidebar.css";
 import { logout } from "../../config/firebase";
+import {AppContext} from '../../context/AppContext'
+
 const RightSidebar = () => {
-  return (
+
+    const {chatUser, messages} = useContext(AppContext)
+    const [msgImages,setMsgImages] =useState([])
+
+    useEffect(() => {
+      let temVar = []
+      messages.map((msg)=>{
+        if (msg.image) {
+          temVar.push(msg.image)
+        }
+      })
+      console.log(temVar)
+    }, [messages]);
+
+  return chatUser ? (
     <div className="rs">
       <div className="rs-profile">
-        <img src={assets.profile_img} alt="" />
+        <img src={chatUser.userData.avatar} alt="" />
         <h3>
-          yared Aregayehu <img src={assets.green_dot} className="dot" alt="" />
+          {chatUser.userData.name}{" "}
+          <img src={assets.green_dot} className="dot" alt="" />
         </h3>
-        <p>Hey, There i am yared using chat app </p>
+        <p>{chatUser.userData.bio}</p>
       </div>
       <hr />
       <div className="rs-media">
         <p>media</p>
-        <img src={assets.pic1} alt="" />
+        <div>
+          {/* <img src={assets.pic1} alt="" />
         <img src={assets.pic2} alt="" />
         <img src={assets.pic3} alt="" />
         <img src={assets.pic4} alt="" />
         <img src={assets.pic1} alt="" />
-        <img src={assets.pic2} alt="" />
+        <img src={assets.pic2} alt="" /> */}
+        </div>
       </div>
-      <button onClick={()=>logout()} >Logout</button>
+      <button onClick={() => logout()}>Logout</button>
+    </div>
+  ) : (
+    <div className="rs">
+      <button onClick={() => logout()}>Logout</button>
     </div>
   );
+
 };
 
 export default RightSidebar;
