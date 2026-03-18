fc4fd0bbda0007bd5faa9d72067b9dc036784898
yared aregayehu <yaredbaby21@gmail.com>
2026-03-18 15:50:08 -0500

    AppContext.jsx updated

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -14,6 +14,7 @@ const AppContextProvider = (props) => {
   const [messageId, setMessagesId] = useState(null);
   const [messages, setMessages] = useState([]);
   const [chatUser, setChatUser] = useState(null);
+  const [chatVisible,setChatVisible] = useState(false)
 
   const loadUserData = async (uid) => {
     try {
@@ -80,6 +81,7 @@ const AppContextProvider = (props) => {
     setMessagesId,
     chatUser,
     setChatUser,
+    chatVisible,setChatVisible
   };
 
   return (
